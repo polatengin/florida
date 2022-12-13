d8d690bf79c5ca719011d02f75e22d2bb3d916b3
Engin Polat <polatengin@hotmail.com>
2022-12-13 23:04:45 +0000

    creating langing page

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/header";
+
+export default () => {
+  return (
+    <>
+      <div className="flex flex-col h-screen">
+        <Header />
+        <main className="grid h-full p-6">
+        </main>
+      </div>
+    </>
+  );
+};

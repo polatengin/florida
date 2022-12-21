9489cdbe7289c06cafdb4c87179ab92cad14f794
Engin Polat <polatengin@hotmail.com>
2022-12-21 17:31:59 +0000

    adding rendergroupitem method

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -10,7 +10,16 @@ type GroupItem = { cell: string, key: ProjectIssueItem, group: ProjectIssueItem[
 
 export default ({project_fields, project_issue_items}: Props) => {
 
-  console.log({project_fields, project_issue_items});
+  const renderGroupItem = (item: GroupItem): ReactNode => {
+    return <button key={item.key.id} className="flex w-8 h-8 z-10" onClick={e => setSelectedGroup(item)} style={{
+      left: item.key.impact * 10 * stepWidth,
+      top: item.key.probability * 10 * stepHeight,
+      borderColor: borderColorList[0]
+    }} title={item.group.length === 0 ? item.key.title : `${item.group.length+1} grouped risks:\n${item.key.id}: ${item.key.title}\n${item.group.map(e => `${e.id}: ${e.title}\n`)}`}>
+      { renderShapeIcon(item.key, item.group.length) }
+      <span>{item.group.length === 0 ? item.key.id : `# ${item.group.length+1}`}</span>
+    </button>;
+  };
 
   return (
   <div>

d96abb19b35ba0b25ede27e49725d6762905eaef
Engin Polat <polatengin@hotmail.com>
2022-12-21 17:29:22 +0000

    adding questionicon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -28,3 +28,9 @@ export const InformationIcon: FunctionComponent<IconProps> = (props: IconProps) 
     <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
   </svg>
 );
+
+export const QuestionIcon: FunctionComponent<IconProps> = (props: IconProps) => (
+  <svg fill="#fff" viewBox="0 0 24 24" stroke="currentColor" style={props.style} className={props.className} onClick={props.onClick}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+  </svg>
+);

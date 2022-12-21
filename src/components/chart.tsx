adcfbc3d74cb61d56368565461762de4e7a20c89
Engin Polat <polatengin@hotmail.com>
2022-12-21 17:32:13 +0000

    adding missing bordercolor list

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -10,6 +10,8 @@ type GroupItem = { cell: string, key: ProjectIssueItem, group: ProjectIssueItem[
 
 export default ({project_fields, project_issue_items}: Props) => {
 
+  const borderColorList = ["", "#4c206d", "#cc0000", "#7a83dd", "#5b88ab", "#005b99", "#4B0082", "#EE82EE"];
+
   const renderGroupItem = (item: GroupItem): ReactNode => {
     return <button key={item.key.id} className="flex w-8 h-8 z-10" onClick={e => setSelectedGroup(item)} style={{
       left: item.key.impact * 10 * stepWidth,

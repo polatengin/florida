947d27837e44650d5a651950c35e1f02691ddc21
Engin Polat <polatengin@hotmail.com>
2022-12-21 17:28:59 +0000

    adding circleicon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -16,3 +16,9 @@ export const FireIcon: FunctionComponent<IconProps> = (props: IconProps) => (
     <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
   </svg>
 );
+
+export const CircleIcon: FunctionComponent<IconProps> = (props: IconProps) => (
+  <svg fill="#fff" viewBox="0 0 24 24" stroke="currentColor" style={props.style} className={props.className} onClick={props.onClick}>
+    <circle cx="12" cy="12" r="11" />
+  </svg>
+);

3fcffc75018adc5060d6b47dc227e7914dffbf28
Engin Polat <polatengin@hotmail.com>
2022-12-21 17:28:04 +0000

    adding missing fields

--- src/services/project_issue_items.ts
+++ src/services/project_issue_items.ts
@@ -7,6 +7,7 @@ type _GitHub_Project_Issue_Item = {
         items: {
           nodes: {
             id: string;
+            createdAt: string;
             content: {
               id: string;
               title: string;
@@ -32,14 +33,43 @@ type _GitHub_Project_Issue_Item = {
 };
 
 export type ProjectIssueItem = {
-  id: string;
+  id: number;
+  cell: string;
+  createdAt: string;
   title: string;
   priority: number;
   impact: number;
   probability: number;
+  isMitigated: boolean;
 };
 
 export default async (projectNumber: number): Promise<ProjectIssueItem[]> => {
+  const getCellName = (impact: number, probability: number): string => {
+    let cellName = "";
+    if (impact <= 3 && impact >= 2 && probability <= 1) {
+      cellName = "High-Low";
+    } else if (impact <= 3 && impact >= 2 && probability <= 2 && probability >= 1) {
+      cellName = "High-Medium";
+    } else if (impact <= 3 && impact >= 2 && probability <= 3 && probability >= 2) {
+      cellName = "High-High";
+    } else if (impact <= 2 && impact >= 1 && probability <= 1) {
+      cellName = "Medium-Low";
+    } else if (impact <= 2 && impact >= 1 && probability <= 2 && probability >= 1) {
+      cellName = "Medium-Medium";
+    } else if (impact <= 2 && impact >= 1 && probability <= 3 && probability >= 2) {
+      cellName = "Medium-High";
+    } else if (impact <= 1 && probability <= 1) {
+      cellName = "Low-Low";
+    } else if (impact <= 1 && probability <= 2 && probability >= 1) {
+      cellName = "Low-Medium";
+    } else if (impact <= 1 && probability <= 3 && probability >= 2) {
+      cellName = "Low-High";
+    } else {
+      cellName = "Unknown";
+    }
+    return cellName;
+  };
+
   const query = `
   {
     viewer {
@@ -47,6 +77,7 @@ export default async (projectNumber: number): Promise<ProjectIssueItem[]> => {
         items(first: 100) {
           nodes {
             id
+            createdAt
             content {
               ... on DraftIssue {
                 id
@@ -95,13 +126,21 @@ export default async (projectNumber: number): Promise<ProjectIssueItem[]> => {
 
   const request = await graphql<_GitHub_Project_Issue_Item>(query);
 
+  console.log(JSON.stringify(request, null, 2));
+
   return request.data.viewer.projectV2.items.nodes.map((item) => {
+    const impact = Number(item.fieldValues.nodes.find((node) => node.field?.name === "Impact")?.number ?? 0);
+    const probability = Number(item.fieldValues.nodes.find((node) => node.field?.name === "Probability")?.number ?? 0);
+
     return {
-      id: item.id,
+      id: Number(item.id),
+      cell: getCellName(impact, probability),
+      createdAt: item.createdAt,
       title: item.content.title,
       priority: Number(item.fieldValues.nodes.find((node) => node.field?.name === "Priority")?.name?.split(" ")[1] ?? 0),
-      impact: Number(item.fieldValues.nodes.find((node) => node.field?.name === "Impact")?.number ?? 0),
-      probability: Number(item.fieldValues.nodes.find((node) => node.field?.name === "Probability")?.number ?? 0),
+      impact,
+      probability,
+      isMitigated: false
     };
   });
 }

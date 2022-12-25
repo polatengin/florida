71c310b1921c98738a73c793f35cf55c62438e25
Engin Polat <polatengin@hotmail.com>
2022-12-25 20:20:22 +0000

    updating to a named component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,10 @@ import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
   )
-}
+};

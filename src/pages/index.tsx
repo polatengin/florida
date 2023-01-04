414789a6355e30d676f1b92da87fc3502b9247e0
Engin Polat <polatengin@hotmail.com>
2023-01-04 20:32:43 +0000

    fixing component references

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import Header from "../components/header";
+import { HeaderComponent } from "../components/header";
 
 export default () => {
   return (
     <>
       <div className="flex flex-col h-screen">
-        <Header />
+        <HeaderComponent />
         <main className="grid h-full p-6">
         </main>
       </div>

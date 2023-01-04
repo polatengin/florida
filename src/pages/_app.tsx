003e8e288bff32ee56ab93a5e8c3669aca75628b
Engin Polat <polatengin@hotmail.com>
2023-01-04 20:31:05 +0000

    converting to default export

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,10 @@ import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from 'next/app'
 
-export const App = ({ Component, pageProps }: AppProps) => {
+export default ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
-  )
+  );
 };
